e233466b5c1a494c7d3f45d0d9d51a8eb05e66a1
shenzehao <1024746383@qq.com>
2021-03-10 09:34:49 +0800

    添加了数据文件

--- src/tensor_tree_CNN.py
+++ src/tensor_tree_CNN.py
@@ -6,27 +6,63 @@ import numpy as np
 
 
 def read_trainx():
-    data=np.load('../Defect_Data/4号树木x.npy')
+    data=np.load('../Defect_Data/4号树木x_8x25.npy')
     data=np.array(data,dtype='float')
     return data
 
-def read_trainy():
+def read_trainy(length):
     label=np.loadtxt('../Defect_Data/label4_onlydefect_20.txt',encoding='utf-8',dtype='int')
     label=np.array(label).reshape(-1)
-    temp=np.zeros(shape=(5476,400),dtype='int')
+    temp=np.zeros(shape=(length,400),dtype='int')
     temp[:,0:400]=label
     return temp
+
+def read_testx():
+    length=[]
+    data1=np.load('../Defect_Data/1号树木x.npy')
+    data1=np.array(data1,dtype='float')
+    length.append(data1.shape[0])
+    data2=np.load('../Defect_Data/2号树木x.npy')
+    data2=np.array(data2,dtype='float')
+    length.append(data2.shape[0])
+    data3=np.load('../Defect_Data/3号树木x.npy')
+    data3=np.array(data3,dtype='float')
+    length.append(data3.shape[0])
+    data=np.concatenate((data1,data2,data3))
+    return data,length
+
+def read_testy(length):
+    label1=np.loadtxt('../Defect_Data/label1_onlydefect_20.txt',encoding='utf-8',dtype='int')
+    label1=np.array(label1).reshape(-1)
+    temp1=np.zeros(shape=(length[0],400),dtype='int')
+    temp1[:,0:400]=label1
+    label2=np.loadtxt('../Defect_Data/label1_onlydefect_20.txt',encoding='utf-8',dtype='int')
+    label2=np.array(label2).reshape(-1)
+    temp2=np.zeros(shape=(length[1],400),dtype='int')
+    temp2[:,0:400]=label2
+    label3=np.loadtxt('../Defect_Data/label1_onlydefect_20.txt',encoding='utf-8',dtype='int')
+    label3=np.array(label3).reshape(-1)
+    temp3=np.zeros(shape=(length[2],400),dtype='int')
+    temp3[:,0:400]=label3
+    Y=np.concatenate((temp1,temp2,temp3))
+    return Y
 
 
 
 def main():
     # 获取数据
     x_train=read_trainx()
-    y_train=read_trainy()
-    x_test=x_train[0:1000]
-    y_test=y_train[0:1000]
-    x_train=x_train[1000:]
-    y_train=y_train[1000:]
+    y_train=read_trainy(x_train.shape[0])
+    x_test,len_temp=read_testx()
+    y_test=read_testy(len_temp)
+
+
+
+    #进一步处理数据
+    # x_test=x_train[0:500]
+    # y_test=y_train[0:500]
+    # x_train=x_train[500:]
+    # y_train=y_train[500:]
 
     model = keras.Sequential()
     model.add(keras.Input(shape=(44)))  # 250x250 RGB images
@@ -45,13 +81,16 @@ def main():
     model.add(layers.Flatten()
 
     )  # 把池化层的输出扁平化为一维数据
-    model.add(Dense(1440))  # 第一层全连接层
-    model.add(Dense(720))  # 第一层全连接层
+    model.add(Dense(1440,activation='softplus'))  # 第一层全连接层
+    model.add(Dense(720,activation='relu'))  # 第一层全连接层
+    config = model.get_config()  # 把model中的信息，solver.prototxt和train.prototxt信息提取出来
+    print(config)
+    # model = model.from_config(config)  # 还回去
     model.add(Dense(400,activation=tf.sigmoid))  # 第二层全连接层
     model.summary()
 
     model.compile(optimizer='adam',loss='mse',metrics=['accuracy'])
-    model.fit(x=x_train,y=y_train, batch_size=200, epochs=10)
+    model.fit(x=x_train,y=y_train, batch_size=200, epochs=2)
     result = model.evaluate(x_test, y_test)
     print('TEST ACC:', result[1])
 
--- src/write_test.py
+++ src/write_test.py
@@ -28,6 +28,9 @@ def readfile(filename):
 
 
 def write_x():
+    """
+    编写4号树木的x轴输入信息
+    """
     ospath = '../Data2/实验室4号树木/'
     ospathnames = [name for name in os.listdir(ospath)
                    if os.path.isdir(os.path.join(ospath, name))]
@@ -50,18 +53,51 @@ def write_x():
                 locat_list[i].append(nums[1])  # 添加新读取的数据
         locat_list = np.array(locat_list).reshape(16)  # 将数据从list类型转换为array类型。
 
-        for i in range(len(filenames)):
+        for i in range(25):
             data_list = readfile(filenames[i])
             data_list = np.concatenate((data_list, locat_list), axis=0)
             outcome.append(data_list)
     outcome = np.array(outcome).reshape(-1, 44)
-    np.save('../Defect_Data/4号树木x.npy', outcome)
+    np.save('../Defect_Data/4号树木x_8x25.npy', outcome)
 
 def write_y():
+    """
+    这个函数不会被调用，用于编写y轴信息
+    """
     label=np.loadtxt('../Defect_Data/label4.txt',encoding='utf-8',dtype='int')
     label=np.array(label).reshape(-1)
     temp=np.zeros(shape=(5476,10000),dtype='int')
     temp[:,0:10000]=label
     pass
 
-write_y()
+def write_test_x():
+    """
+    编写1-3号树木的输入x信息，秩序改写osname和np.save的文件名
+    """
+    osname = '../Data/实验室3号树木/'
+    filenames = [name for name in os.listdir(osname)
+                 if os.path.isfile(os.path.join(osname, name)) and
+                 (name.startswith('树莓派') or
+                  name.startswith('20') or
+                  name.startswith('手按'))]
+    for i in range(len(filenames)):
+        filenames[i] = osname + filenames[i]
+    locat_filename = osname + 'location.txt'
+    locat_list = [[] for i in range(8)]
+    with open(locat_filename, 'r', encoding='utf-8') as file_to_read:
+        for i in range(8):
+            lines = file_to_read.readline()  # 整行读取数据
+            nums = re.split(',|\n', lines)
+            locat_list[i].append(nums[0])  # 添加新读取的数据
+            locat_list[i].append(nums[1])  # 添加新读取的数据
+    locat_list = np.array(locat_list).reshape(16)  # 将数据从list类型转换为array类型。
+    outcome=[]
+    for i in range(len(filenames)):
+        data_list = readfile(filenames[i])
+        data_list = np.concatenate((data_list, locat_list), axis=0)
+        outcome.append(data_list)
+    outcome = np.array(outcome).reshape(-1, 44)
+    np.save('../Defect_Data/3号树木x.npy', outcome)
+    pass
+
+write_x()
